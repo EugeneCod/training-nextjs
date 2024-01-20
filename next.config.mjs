f02e7ba9b6e781a936c38b079acbebd675268f9b
EugeneKGen <eugene_korobkin@mail.ru>
2024-01-20 21:22:07 +0300

    feat: add basePath opption

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+/* Если вы размещаете свое приложение на корневом уровне URL, например yourblog.com/, проблем 
+не возникнет. Вы можете подключить пользовательский домен к своим страницам github, и это 
+будет работать. Но если вы хотите иметь возможность размещать свой блог, используя свой URL 
+на github, например, https://<username>.github.io/<repository-name>/ мы должны сообщить 
+нашему приложению, что базовый путь - это /<repository-name>. Это исправит все относительные URL. */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', 
+  basePath: "/training-nextjs", // задать базовый путь.
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
